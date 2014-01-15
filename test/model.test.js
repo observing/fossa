e9418b14ffb325ea48a13add449cca66fbae9cd2
Swaagie <info@martijnswaagman.nl>
2014-01-16 00:33:53 +0100

    [minor] take various option passing into account

--- test/model.test.js
+++ test/model.test.js
@@ -68,37 +68,87 @@ describe('Fossa Model', function () {
     expect(model.id).to.be.an.instanceof(ObjectID);
   });
 
-  it('can be stored in MongoDB by sync with urlRoot property', function (done) {
-    var model = new fossa.Model({ username: 'test' });
+  describe('#sync', function () {
+    it('can be stored in MongoDB by sync with urlRoot property', function (done) {
+      var model = new fossa.Model({ username: 'test' });
 
-    model.urlRoot = 'users';
-    model.use('fossa').sync().done(function synced(err, result) {
-      db.collection('users').findOne({ _id: model.id }, function (err, item) {
-        expect(err).to.equal(null);
-        expect(item).to.have.property('_id');
-        expect(item._id.toString()).to.equal(model.id.toString());
-        expect(item).to.have.property('username', 'test');
-        done();
-      });
-    });
-  });
-
-  it('switches to PUT if the model with ObjectID exists', function (done) {
-    var id = new ObjectID
-      , model = new fossa.Model({ _id: id, username: 'test' });
-
-    model.urlRoot = 'users';
-    model.use('fossa').sync().done(function synced(err, result) {
-      model.set('username', 'changed');
-      model.sync().done(function synced(err, result) {
-        db.collection('users').findOne({ _id: id }, function (err, item) {
+      model.urlRoot = 'users';
+      model.use('fossa').sync().done(function synced(err, result) {
+        db.collection('users').findOne({ _id: model.id }, function (err, item) {
           expect(err).to.equal(null);
           expect(item).to.have.property('_id');
-          expect(item._id.toString()).to.equal(id.toString());
-          expect(item).to.have.property('username', 'changed');
+          expect(item._id.toString()).to.equal(model.id.toString());
+          expect(item).to.have.property('username', 'test');
           done();
         });
       });
+    });
+
+    it('switches to PUT if the model with ObjectID exists', function (done) {
+      var id = new ObjectID
+        , model = new fossa.Model({ _id: id, username: 'test' });
+
+      model.urlRoot = 'users';
+      model
+        .use('fossa')
+        .sync()
+        .done(function synced(err, result) {
+          model.set('username', 'changed');
+          model.sync().done(function synced(err, result) {
+            db.collection('users').findOne({ _id: id }, function (err, item) {
+              expect(err).to.equal(null);
+              expect(item).to.have.property('_id');
+              expect(item._id.toString()).to.equal(id.toString());
+              expect(item).to.have.property('username', 'changed');
+              done();
+            });
+          });
+      });
+    });
+
+    it('returns an error when updating a undefined model in MongoDB', function (done) {
+      var model = new fossa.Model({ username: 'test' });
+
+      model.urlRoot = 'users';
+      model.use('fossa').sync('update').done(function synced(err, result) {
+        expect(err).to.be.instanceof(Error);
+        expect(err).to.have.property('name', 'MongoError');
+        expect(err).to.have.property('err', 'Mod on _id not allowed');
+        expect(err.message).to.equal('Mod on _id not allowed');
+        done();
+      });
+    });
+
+    it('can pass MongoDB options, per example wtimeout', function (done) {
+      var model = new fossa.Model({ fn: function serialize() { return true; } });
+
+      model.urlRoot = 'users';
+      model
+        .use('fossa')
+        .sync({ serializeFunctions: true })
+        .done(function synced(err, result) {
+          db.collection('users').findOne({ _id: model.id }, function (err, item) {
+            expect(item).to.have.property('fn');
+            expect(item.fn).to.have.property('code', 'function serialize() { return true; }');
+            expect(item.fn).to.have.property('scope');
+            done();
+          });
+        });
+    });
+
+    it('can pass MongoDB options to PUT, per example upsert', function (done) {
+      var model = new fossa.Model({ username: 'upsert' });
+
+      model.urlRoot = 'users';
+      model
+        .use('fossa')
+        .sync('update', { upsert: true })
+        .done(function synced(err, result) {
+          db.collection('users').findOne({ _id: model.id }, function (err, item) {
+            expect(item).to.have.property('username', 'upsert');
+            done();
+          });
+        });
     });
   });
 });
